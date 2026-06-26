cdcacd5187f73c23c0ffa5f9dc847758f1d4bb29
morgana <2924028126@qq.com>
2026-06-26 16:26:36 +0800

    刷题

--- project/server/utils/slowClient.py
+++ project/server/utils/slowClient.py
@@ -1,23 +1,19 @@
 import socket
-import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 8080))
 
-s.sendall(b"hello\n")
+s.sendall(b"big response please\n")
+s.shutdown(socket.SHUT_WR)
 
-print("sent request, now sleep without reading...")
-time.sleep(10)
-
-print("start reading...")
 total = 0
-while True:
-    data = s.recv(4096)
-    if not data:
-        break
-    total += len(data)
-    if total % (1024 * 1024) < 4096:
-        print("received", total, "bytes")
 
-print("done, total =", total)
+while True:
+    chunk = s.recv(4096)
+    if not chunk:
+        break
+    total += len(chunk)
+
+print("total received:", total)
+
 s.close()
